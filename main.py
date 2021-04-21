0d60464ff64edc38dc25173fc5b29e79fc990228
davidhoang <lhoang@szeta.ecs.tuwien.ac.at>
2021-04-21 16:56:54 +0200

    v1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from dataloader import data_preparation
-from models import WizardNet
-from pipelines import *
-import torch
+
 import argparse
+from network import wizard
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch MNIST Training')
@@ -22,32 +21,10 @@ if __name__ == "__main__":
     parser.add_argument('--split-fraction', type=float, default =0.1,
                         help='fraction between train/validation set')
 
-    use_cuda = torch.cuda.is_available()
-    torch.manual_seed(args.seed)
-    device = torch.device("cuda" if use_cuda else "cpu")
-    train_data, val_data, test_data, train_label, val_label = data_preparation()
-    model = WizardNet
-    #Training
-    print("|| Starting the training phase")
-    print("|| Number of epochs:", args.epochs)
-    if use_cuda: print("|| Training model on GPU\n")
-    if not os.path.exists(args.out_dir):
-        os.mkdir(args.out_dir)
+    
+    
+    model = wizard()
+    model.training()
 
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-    for epoch in range(1, args.epochs + 1):
-        train(args.log_interval, model, device, train_data, optimizer, epoch)
-        val(model, device, val_set)
-    test(model, device, val_data)
-    torch.save(model.state_dict(), args.out_dir +  "model.pt")
-#evaluating
-    model.train()
-    model.test()
 
-class model(self):
-    def __int__(self):
-        pass
-    def __train__(self):
 
-    def __test__(self):
-        pass
